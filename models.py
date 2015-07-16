111f46db9ed4415bb1e6acafe93fdd3a36fc6c00
Ezra <ezrazigmond@gmail.com>
2015-07-16 19:05:18 -0400

    Created peewee models

---
+++ models.py
@@ -0,0 +1,27 @@
+from peewee import *
+
+db = SqliteDatabase('memes.db')
+
+class MSModel(Model):
+    class Meta:
+        database = db
+
+class MemeType(MSModel):
+    id = PrimaryKeyField()
+
+    meme_type_name = CharField (
+        max_length=128, unique = True
+    )
+
+class Meme(MSModel):
+    id = PrimaryKeyField()
+
+    top_text = CharField(
+        max_length=128
+    )
+    bottom_text = CharField(
+        max_length=128
+    )
+    meme_type_id = ForeignKeyField(
+        MemeType, relate_name='memes'
+    )
