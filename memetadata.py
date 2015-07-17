8e6b604ab48ee68c7978304b3d0e8bb7ea7d50a5
Ezra <ezrazigmond@gmail.com>
2015-07-16 22:38:32 -0400

    Now adds memes to database

--- memetadata.py
+++ memetadata.py
@@ -1,6 +1,8 @@
 """Requests meme metadata (memetadata) from memegenerator's API
 """
 
+from models import *
+
 import requests
 
 base_data_uri = "http://version1.api.memegenerator.net/Instances_Select_ByNew"
@@ -10,13 +12,22 @@ meme_name_dictionary = {'Y-U-No': 2287719, 'Futurama-Fry': 1461840, 'Success-Kid
                         'Philosoraptor': 733671, 'One-Does-Not-Simply-A': 1146447,
                         'Bad-Luck-Brian-Meme': 653934, 'Good-Guy-Greg': 338941}
 
-for meme, count in meme_name_dictionary.iteritems():
-	get_memetadata(meme, count)
+initialize_db()
 
 def get_memetadata(meme_name, count):
+	meme_type = MemeType.get_or_create(meme_type_name = meme_name)[0]
 
-	for i in range(0, count/24):
+	for i in range(0, count // 24):
 		payload = {'languageCode': 'en', 'pageIndex': i, 'pageSize': 24, 'urlName': meme_name}
 		jsonResponse = requests.get(base_data_uri, params = payload).json()
+		
+		for meme in jsonResponse['result']:
+			Meme.create (
+				top_text=meme['text0'],
+				bottom_text=meme['text1'],
+				score=meme['totalVotesScore'],
+				meme_type_id=meme_type
+			)
 
-		"""parse the JSON and write to DB or something here"""
+for meme, count in meme_name_dictionary.items():
+	get_memetadata(meme, count)
--- models.py
+++ models.py
@@ -2,6 +2,10 @@ from peewee import *
 
 db = SqliteDatabase('memes.db')
 
+def initialize_db():
+    db.connect()
+    db.create_tables([MemeType, Meme], safe=True)
+
 class MSModel(Model):
     class Meta:
         database = db
@@ -10,18 +14,21 @@ class MemeType(MSModel):
     id = PrimaryKeyField()
 
     meme_type_name = CharField (
-        max_length=128, unique = True
+        max_length=128, unique = True, index = True
     )
 
 class Meme(MSModel):
     id = PrimaryKeyField()
 
     top_text = CharField(
-        max_length=128
+        max_length=128, null=True
     )
     bottom_text = CharField(
-        max_length=128
+        max_length=128, null=True
     )
     meme_type_id = ForeignKeyField(
-        MemeType, relate_name='memes'
+        MemeType, related_name='memes'
+    )
+    score = IntegerField(
+        default=0
     )
